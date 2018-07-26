8293a56b06807847ffcabdb998b8e730bfd65751
Aaron Marburg <aaron@apl.washington.edu>
2018-07-25 17:35:56 -0700

    Trying to make a template to assist helper construction.

--- include/libbmsdi/helpers.h
+++ include/libbmsdi/helpers.h
@@ -11,24 +11,50 @@ extern "C" {
 // These are convenience wrappers which properly format / set up
 // the arguments to some of the commands in the BM SDK.
 
+#define HELPER_NO_PARAM( name, category, param ) \
+  inline BMSDIBuffer *bmAdd##name( BMSDIBuffer *buffer, uint8_t dest ) { \
+    BMSDIMessage *msg = bmAddConfigMessage( buffer, dest, category, param, BM_OP_ASSIGN, BM_TYPE_VOID, 0); \
+    return (msg ? buffer : NULL); \
+  } \
+  inline BMSDIBuffer *bm##name( uint8_t dest ) { \
+    return bmAdd##name( bmNewBuffer(), dest ); \
+  }
+
+
+
+#define HELPER_ONE_PARAM( name, category, param, p1type, p1const ) \
+  inline BMSDIBuffer *bmAdd##name( BMSDIBuffer *buffer, uint8_t dest, p1type p1 ) { \
+    BMSDIMessage *msg = bmAddConfigMessage( buffer, dest, category, param, BM_OP_ASSIGN, p1const, 1); \
+    if( !msg ) return NULL; \
+    return (msg ? buffer : NULL); \
+  } \
+  inline BMSDIBuffer *bm##name( uint8_t dest, p1type p1 ) { \
+    return bmAdd##name( bmNewBuffer(), dest, p1 ); \
+  }
+
+
+
+
 // Message 0.0:  Set focus
-inline BMSDIBuffer *bmAddFocus( BMSDIBuffer *buffer, uint8_t dest, float focus ) {
-  BMSDIMessage *msg = bmAddConfigMessage( buffer,
-                                          dest,
-                                          BM_CAT_LENS,
-                                          BM_PARAM_FOCUS,
-                                          BM_OP_ASSIGN,
-                                          BM_TYPE_FIXED16,
-                                          1 );
-  if( !msg ) return NULL;
+// inline BMSDIBuffer *bmAddFocus( BMSDIBuffer *buffer, uint8_t dest, float focus ) {
+//   BMSDIMessage *msg = bmAddConfigMessage( buffer,
+//                                           dest,
+//                                           BM_CAT_LENS,
+//                                           BM_PARAM_FOCUS,
+//                                           BM_OP_ASSIGN,
+//                                           BM_TYPE_FIXED16,
+//                                           1 );
+//   if( !msg ) return NULL;
+//
+//   bmConfigWriteFixed16( msg, focus );
+//   return buffer;
+// }
+//
+// inline BMSDIBuffer *bmFocus( uint8_t dest, float focus ) {
+//   return bmAddFocus( bmNewBuffer(), dest, focus );
+// }
 
-  bmConfigWriteFixed16( msg, focus );
-  return buffer;
-}
-
-inline BMSDIBuffer *bmFocus( uint8_t dest, float focus ) {
-  return bmAddFocus( bmNewBuffer(), dest, focus );
-}
+HELPER_ONE_PARAM( Focus, BM_CAT_LENS, BM_PARAM_FOCUS, float, BM_TYPE_FIXED16 )
 
 // Message 0.0:  Increment focus
 inline BMSDIBuffer *bmAddFocusOffset( BMSDIBuffer *buffer, uint8_t dest, float inc ) {
@@ -47,19 +73,20 @@ inline BMSDIBuffer *bmAddFocusOffset( BMSDIBuffer *buffer, uint8_t dest, float i
 
 
 // Message 0.1:  Instantantaneous Autofocus
-inline BMSDIBuffer *bmAddInstantaneousAutofocus( BMSDIBuffer *buffer, uint8_t dest ) {
-  BMSDIMessage *msg = bmAddConfigMessage( buffer, dest,
-                                          BM_CAT_LENS,
-                                          BM_PARAM_INST_AUTOFOCUS,
-                                          BM_OP_ASSIGN,
-                                          BM_TYPE_VOID,
-                                          0 );
-  return (msg ? buffer : NULL);
-}
+// inline BMSDIBuffer *bmAddInstantaneousAutofocus( BMSDIBuffer *buffer, uint8_t dest ) {
+//   BMSDIMessage *msg = bmAddConfigMessage( buffer, dest,
+//                                           BM_CAT_LENS,
+//                                           BM_PARAM_INST_AUTOFOCUS,
+//                                           BM_OP_ASSIGN,
+//                                           BM_TYPE_VOID,
+//                                           0 );
+//   return (msg ? buffer : NULL);
+// }
+HELPER_NO_PARAM( InstantaneousAutofocus, BM_CAT_LENS, BM_PARAM_INST_AUTOFOCUS )
 
-inline BMSDIBuffer *bmInstantaneousAutofocus( uint8_t dest ) {
-  return bmAddInstantaneousAutofocus( bmNewBuffer(), dest );
-}
+// inline BMSDIBuffer *bmInstantaneousAutofocus( uint8_t dest ) {
+//   return bmAddInstantaneousAutofocus( bmNewBuffer(), dest );
+// }
 
 
 //=== Message 0.4:  Ordinal Aperture
